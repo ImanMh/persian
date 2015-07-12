419ea4fd07b6a1e764fe7b3abac9ca8b60726849
Iman Mohamadi <iman.mohamadi.dev@gmail.com>
2015-07-12 18:31:08 +0430

    numerical convertion added

--- textToNumberDate/main.py
+++ textToNumberDate/main.py
@@ -1,5 +1,27 @@
+import re
 
 
 
-class TextToNumberDate ():
-    pass
+class TextToDate ():
+    @staticmethod
+    def convert(text):
+        #matches 23/11/1345, 31/11/3234
+        standardNumericPattern = '^([0-2][0-9]|3[0-1])\/([0][0-9]|1[0-2])\/[0-9]{0,4}$'
+        #matches all standards plus 3/3/3001, 3/04/2133
+        noneStandardNumericPattern = '(([0-2][0-9]|3[0-1])|[0-9])\/(([0][0-9]|1[0-2])|[0-9])\/[0-9]{0,4}';
+
+        if (re.match(standardNumericPattern, text)):
+            return text
+        if (re.match(noneStandardNumericPattern, text)):
+            dateSections = text.split('/')
+            for i, section in enumerate(dateSections):
+                if (i == 2):
+                    while len(dateSections[i]) < 4:
+                        dateSections[i] = "0" + dateSections[i]
+                if (len(section) < 2):
+                    dateSections[i] = "0" + section
+            delimeter = '/'
+            return delimeter.join(dateSections)
+
+
+        return 1
--- textToNumberDate/main_test.py
+++ textToNumberDate/main_test.py
@@ -1,15 +1,25 @@
 import unittest
 from main import *
 
-class DummyTest (unittest.TestCase):
+class DummyTests (unittest.TestCase):
 
     def test_obvious (self):
         self.assertEqual(1, 1)
 
     def test_creating_a_basic_app_object (self):
-        converter = TextToNumberDate()
-        self.assertIsInstance(converter, TextToNumberDate)
+        converter = TextToDate()
+        self.assertIsInstance(converter, TextToDate)
 
+class convertNumericInputs (unittest.TestCase):
+    def test_passing_a_numerical_date_return_same(self):
+        converter = TextToDate()
+        date = converter.convert("02/03/1384")
+        self.assertEqual(date, "02/03/1384")
+
+    def test_passing_none_standard_numerical_returns_standard_numerical(self):
+        converter = TextToDate()
+        self.assertEqual(converter.convert("2/3/1345"), "02/03/1345")
+        self.assertEqual(converter.convert("2/3/45"), "02/03/0045")
 
 if __name__ == '__main__':
     unittest.main()
