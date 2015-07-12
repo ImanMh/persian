1708f356671d1b9adc741456ab9db60c8ef2fb64
Iman Mohamadi <iman.mohamadi.dev@gmail.com>
2015-07-13 02:26:23 +0430

    full support for two digit numbers

--- textToNumberConverter/main.py
+++ textToNumberConverter/main.py
@@ -3,6 +3,54 @@ import re
 
 
 class TextToNumber ():
+
+    zeroToTwenty = {
+        'sefr': 0,
+        'yek': 1,
+        'do': 2,
+        'se': 3,
+        'chahar': 4,
+        'panj': 5,
+        'shesh': 6,
+        'haft': 7,
+        'hasht': 8,
+        'noh': 9,
+        'dah': 10,
+        'yazdah': 11,
+        'davazdah': 12,
+        'sizdah': 13,
+        'chahardah': 14,
+        'panzdah': 15,
+        'shanzdah': 16,
+        'hefdah': 17,
+        'hejdah': 18,
+        'noonzdah': 19,
+        'bist': 20
+    }
+
+    twoDigitNumbers = {
+        'bist': 20,
+        'si': 30,
+        'chehel': 40,
+        'panjah': 50,
+        'shast': 60,
+        'haftad': 70,
+        'hashtad': 80,
+        'navad': 90,
+    }
+
     @staticmethod
     def convert(text):
-        return 1
+        if (len(text.split(' ')) == 1 ):
+            if (text in TextToNumber.zeroToTwenty):
+                return TextToNumber.zeroToTwenty[text]
+            if (text in TextToNumber.twoDigitNumbers):
+                return TextToNumber.twoDigitNumbers[text]
+        else:
+            sections = text.split(' ')
+            numberParts = []
+            numberParts.append(TextToNumber.twoDigitNumbers[sections[0]])
+            numberParts.append(TextToNumber.zeroToTwenty[sections[2]])
+            return numberParts[0] + numberParts[1]
+
+        raise ValueError('Could not detect the input you just passed: ' + text)
--- textToNumberConverter/main_test.py
+++ textToNumberConverter/main_test.py
@@ -3,15 +3,49 @@ from main import *
 
 class DummyTests (unittest.TestCase):
 
-    def test_obvious (self):
+    def test_obvious(self):
         self.assertEqual(1, 1)
 
-    def test_creating_a_basic_app_object (self):
+    def test_creating_a_basic_app_object(self):
         converter = TextToNumber()
         self.assertIsInstance(converter, TextToNumber)
 
-class convertNumericInputs (unittest.TestCase):
-    pass
+
+class convertUpToTwenty (unittest.TestCase):
+
+    def test_can_convert_one(self):
+        self.assertEqual(TextToNumber.convert('yek'), 1);
+
+    def test_can_convert_zero(self):
+        self.assertEqual(TextToNumber.convert('sefr'), 0);
+
+    def test_can_conver_ten(self):
+        self.assertEqual(TextToNumber.convert('noonzdah'), 19);
+
+    def test_can_conver_twenty(self):
+        self.assertEqual(TextToNumber.convert('bist'), 20);
+
+
+class convertTwentyToThirty (unittest.TestCase):
+
+    def test_can_convert_two_part_two_digit_number(self):
+        self.assertEqual(TextToNumber.convert('bist o yek'), 21)
+
+    def test_can_convert_one_part_two_digit_numbers(self):
+        self.assertEqual(TextToNumber.convert('si'), 30)
+
+    def test_can_convert_ninty_nine(self):
+        self.assertEqual(TextToNumber.convert('navad o noh'), 99)
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
