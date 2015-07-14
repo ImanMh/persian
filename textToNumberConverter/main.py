0c1d0ef55b31bf993c5e25a4476d8717f1ecd1e0
Iman Mohamadi <iman.mohamadi.dev@gmail.com>
2015-07-14 12:27:13 +0430

    the entire app refactored to make the results based on an incremental approach

--- textToNumberConverter/main.py
+++ textToNumberConverter/main.py
@@ -4,6 +4,72 @@ import re
 
 class TextToNumber ():
 
+
+    textNumbers = [
+        {
+            'sefr': 0,
+            'yek': 1,
+            'do': 2,
+            'se': 3,
+            'chahar': 4,
+            'panj': 5,
+            'shesh': 6,
+            'haft': 7,
+            'hasht': 8,
+            'noh': 9,
+            'dah': 10,
+        },
+        {
+            'bist': 20,
+            'si': 30,
+            'chehel': 40,
+            'panjah': 50,
+            'shast': 60,
+            'haftad': 70,
+            'hashtad': 80,
+            'navad': 90,
+        },
+        {
+            'sad': 100,
+            'devist': 200,
+            'sisad': 300,
+            'chaharsad': 400,
+            'pansad': 500,
+            'sheshsad': 600,
+            'haftsad': 700,
+            'hashtsad': 800,
+            'nohsad': 900
+        },
+    ]
+
+
+    oneDigitNumbers = {
+        'sefr': 0,
+        'yek': 1,
+        'do': 2,
+        'se': 3,
+        'chahar': 4,
+        'panj': 5,
+        'shesh': 6,
+        'haft': 7,
+        'hasht': 8,
+        'noh': 9,
+        'dah': 10,
+    }
+
+    tenToNineteen = {
+        'dah': 10,
+        'yazdah': 11,
+        'davazdah': 12,
+        'sizdah': 13,
+        'chahardah': 14,
+        'panzdah': 15,
+        'shanzdah': 16,
+        'hefdah': 17,
+        'hejdah': 18,
+        'noonzdah': 19,
+    }
+
     zeroToTwenty = {
         'sefr': 0,
         'yek': 1,
@@ -39,18 +105,42 @@ class TextToNumber ():
         'navad': 90,
     }
 
+    threeDigitNumbers = {
+        'sad': 100,
+        'devist': 200,
+        'sisad': 300,
+        'chaharsad': 400,
+        'pansad': 500,
+        'sheshsad': 600,
+        'haftsad': 700,
+        'hashtsad': 800,
+        'nohsad': 900
+    }
+
     @staticmethod
     def convert(text):
-        if (len(text.split(' ')) == 1 ):
-            if (text in TextToNumber.zeroToTwenty):
-                return TextToNumber.zeroToTwenty[text]
-            if (text in TextToNumber.twoDigitNumbers):
-                return TextToNumber.twoDigitNumbers[text]
-        else:
-            sections = text.split(' ')
-            numberParts = []
-            numberParts.append(TextToNumber.twoDigitNumbers[sections[0]])
-            numberParts.append(TextToNumber.zeroToTwenty[sections[2]])
-            return numberParts[0] + numberParts[1]
+        splitted = text.split(' o ')
+        numOfWords = len(splitted)
+        firstWord = splitted[0]
+        numOfDigits = None
+
+        if (text in TextToNumber.tenToNineteen):
+            return TextToNumber.tenToNineteen[text]
+
+        if (firstWord in TextToNumber.threeDigitNumbers):
+            numOfDigits = 3
+        if (firstWord in TextToNumber.twoDigitNumbers):
+            numOfDigits = 2
+        if (firstWord in TextToNumber.oneDigitNumbers):
+            numOfDigits = 1
+
+        results = 0
+        currentDigit = numOfDigits
+        for i, split in enumerate(splitted):
+            print(numOfDigits, type(numOfDigits), text)
+            results += TextToNumber.textNumbers[currentDigit - 1][split]
+            currentDigit -= 1
+
+        return results
 
         raise ValueError('Could not detect the input you just passed: ' + text)
